af501a7ea07402a1f2c1c96bddd7bb4ae903be52
jyekang <janicekangaroo@gmail.com>
2023-07-20 19:16:00 -0700

    home page styled

--- tickit/src/Components/Home.jsx
+++ tickit/src/Components/Home.jsx
@@ -3,11 +3,15 @@ const Home = () => {
 
     return (
    
-            <div className='home route-body'>
+            <div className='home'>
                 <h1>Welcome</h1> 
-                <div className="home-links">
-                    <NavLink to="venues">Venues</NavLink>
-                    <NavLink to="events">Events</NavLink>
+                <div>
+                    
+                <div className='home-L1'>Featured Events:</div>
+                <div className='home-L2'>JULY 25-30</div>
+                <div className='home-L3'>MANCHESTER ORCHESTRA <br/> & JIMMY EAT WORLD</div>
+                <NavLink to="venues" className='home-nav-link'>Venues</NavLink>
+                <NavLink to="events" className='home-nav-link'>Events</NavLink>
                 </div>
             </div>
     )
