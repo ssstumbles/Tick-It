1b5f8627bf91420cc6c7ac48c1a795d7264b77d5
Andrew <mullinsa428@gmail.com>
2023-07-20 20:47:29 -0400

    unsure what I changed

--- tickit/src/Components/LoginPortal.jsx
+++ tickit/src/Components/LoginPortal.jsx
@@ -23,11 +23,9 @@ const LoginPortal = () => {
         password: '',
     }
     
-    
     const [buttonStatus, setButtonStatus] = useState(true);
     const [formState, setFormState] = useState(default_input)
     const navigate = useNavigate();
-
 
     const handleChange = (e) => {
         setFormState({...formState, [e.target.id]: e.target.value})
@@ -53,5 +51,4 @@ const LoginPortal = () => {
         </div>
     )
 }
-
 export default LoginPortal
